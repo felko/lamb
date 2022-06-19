ec96923f31772efa5a61cbe07759954f952fb5da
felko <baxbaxwalanuksiwe@gmail.com>
2022-06-19 12:15:54 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A small functional programming language";
+
+  inputs = {
+    naersk.url = "github:nmattia/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in {
+        defaultPackage = naersk-lib.buildPackage ./.;
+
+        defaultApp = {
+          type = "app";
+          program = "${self.defaultPackage."${system}"}/bin/${
+              self.defaultPackage."${system}".pname
+            }";
+        };
+      });
+}
